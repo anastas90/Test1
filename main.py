ef6bca3abd670ca1f595eea8b51022eab2dfa1e4
anastas90 <anastas.sanina@gmail.com>
2013-02-16 21:31:57 +0700

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from random import randint
+
+f = open('ttt.dat')
+
+
+def main():
+    i = 5
+    denominator = 0
+    print (i + 2) / denominator
+    print 'hello! %d' % randint(2, 200)
+
+if __name__ == '__main__':
+    main()
